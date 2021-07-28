41ad3dc723883dce90e6258cf70c5bd8a075debf
이지호 <cctv1998@naver.com>
2021-07-29 00:50:02 +0900

    Style : 사용자 정보 css

--- cafenual-front/src/layouts/Header/Side/index.js
+++ cafenual-front/src/layouts/Header/Side/index.js
@@ -7,7 +7,8 @@ import { FiHome,FiUsers } from 'react-icons/fi';
 const Sidemenu = () => {
   return (
     <div id="sidemenu">
-      <div className="selected-menu"><FiHome size="20"/>홈</div>
+      <div className="selected-menu"><FiHome size="20"/><Link to="/dash">홈</Link></div>
+
       <div className="non-selected-menu"><FiUsers size="20"/><Link to="/dash/user">사용자 정보</Link></div>
 
     </div>
--- cafenual-front/src/pages/Dash/User/index.js
+++ cafenual-front/src/pages/Dash/User/index.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import Sidemenu from "./../../../layouts/Header/Side/index";
 import "./style.scss";
 const UserInfo = () => {
-
-
   const [inputs, setinputs] = useState({
     email: "dlwlgh123@naver.com",
     name: "이지호",
   });
   const [user, setUser] = useState({
-    email:"dlwlgh123@naver.com",
+    email: "dlwlgh123@naver.com",
     name: "이지호",
   });
   const handleOnChange = (e) => {
@@ -22,26 +20,64 @@ const UserInfo = () => {
 
   const handleOnClick = () => {
     setUser({
-        email:inputs.email,
-        name:inputs.name
-    })
-      setOpenemail(false)
-      setOpenname(false)
-}
+      email: inputs.email,
+      name: inputs.name,
+    });
+    setOpenemail(false);
+    setOpenname(false);
+  };
 
   const [Openemail, setOpenemail] = useState(false);
   const [Openname, setOpenname] = useState(false);
-
 
   return (
     <>
       <Sidemenu />
       <div className="user-container">
-        <div className="title">사용자 정보</div>
-        <div className="info-box">
-          <div>이메일 : {user.email}</div>
-          <div>
-            <button onClick={() => setOpenemail(!Openemail)}>
+        <form>
+          <div className="title">사용자 정보</div>
+          <div className="info-box">
+            <div className="info-elements"> 이메일</div>
+            <div className="ans-info">
+              <input type="text" value={user.email} />
+            </div>
+          </div>
+          <div className="info-box">
+            <div className="info-elements">이름</div>
+            <div className="non-ans-info">{user.name}</div>
+          </div>
+          <div className="info-box">
+            <div className="info-elements">현재 비밀번호</div>
+            <div className="ans-info">
+              <input type="password" />
+            </div>
+            <div className="info-elements">새 비밀번호</div>
+            <div className="ans-info">
+              <input type="password" />
+            </div>
+            <div className="info-elements">비밀번호 다시 입력</div>
+            <div className="ans-info">
+              <input type="password" />
+            </div>
+          </div>
+          <div className="submit-box">
+            <input
+              type="submit"
+              className="save-user-info"
+              value="변경 사항 저장하기"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default UserInfo;
+
+{
+  /* <div>
+            <button className="buttons" onClick={() => setOpenemail(!Openemail)}>
               이메일 수정하기
             </button>
           </div>
@@ -56,27 +92,21 @@ const UserInfo = () => {
             </div>
           ) : (
             <></>
-          )}
-        </div>
-        <div className="info-box">
-          <div>이름 : {user.name}</div>
-          <div>
-            <button onClick={() => setOpenname(!Openname)}>
+          )} */
+}
+
+{
+  /* <div>
+            <button className="buttons" onClick={() => setOpenname(!Openname)}>
               이름 수정하기
             </button>
             {Openname === true ? (
               <div>
                 <input type="text" name="name" placeholder="이름" onChange={handleOnChange} />
-                <button onClick={handleOnClick}>변경완료</button>
+                <button className="buttons" onClick={handleOnClick}>변경완료</button>
               </div>
             ) : (
               <></>
             )}
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default UserInfo;
+          </div> */
+}
